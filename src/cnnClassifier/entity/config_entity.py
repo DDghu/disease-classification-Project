965adf9c9c30bd1890f3aaf875ba61855b6cb22a
Deepjoy Das <114580124+DDghu@users.noreply.github.com>
2024-01-29 00:11:45 +0530

    prepare base model done

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -7,3 +7,14 @@ class DataIngestionConfig:     # It's a return type we take this from config.yam
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
